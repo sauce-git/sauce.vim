1204d398985d8f44331465c6234e8598d665fb15
sauce-Git <dadad1004@naver.com>
2024-09-01 23:56:54 +0900

    update keymap

--- lua/sauce/core/keymaps.lua
+++ lua/sauce/core/keymaps.lua
@@ -6,18 +6,8 @@ local keymap = vim.keymap -- for conciseness
 ---------------------
 -- General Keymaps -------------------
 
--- use jk to exit insert mode
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
--- delete single character without copying into register
--- keymap.set("n", "x", '"_x')
-
--- increment/decrement numbers
--- keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
--- keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
@@ -34,5 +24,13 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
+-- terminal keymaps
 keymap.set("n", "<leader>tt", "<cmd>botright 10split term://zsh<CR>", { desc = "Open terminal in split window" }) -- open terminal in split window
 keymap.set("t", "<C-q>", "<C-\\><C-n>", { desc = "Exit terminal" }) -- exit terminal
+
+-- buffer keymaps
+keymap.set("n", "<leader>ba", '<C-6>', { desc = "Switch to previous buffer" }) -- switch to previous buffer
+keymap.set("n", "<leader>bn", '<cmd>bn<CR>', { desc = "Switch to next buffer" }) -- switch to next buffer
+keymap.set("n", "<leader>bp", '<cmd>bp<CR>', { desc = "Switch to previous buffer" }) -- switch to previous buffer
+keymap.set("n", "<leader>bd", '<cmd>bd<CR>', { desc = "Close current buffer" }) -- close current buffer
+keymap.set("n", "<leader>bl", '<cmd>ls<CR>', { desc = "List all buffers" }) -- list all buffers
