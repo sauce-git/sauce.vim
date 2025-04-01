e95c2ecc353e104963b74594c247b7caf9f7e985
sauce <dadad1004@naver.com>
2025-04-01 09:59:35 +0900

    add mcp feat

--- lua/sauce/lazy.lua
+++ lua/sauce/lazy.lua
@@ -13,15 +13,17 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(
-	{ { import = "sauce.plugins" }, { import = "sauce.plugins.lsp" }, { import = "sauce.plugins.colorscheme" } },
-	{
-		checker = {
-			enabled = true,
-			notify = false,
-		},
-		change_detection = {
-			-- notify = false,
-		},
-	}
-)
+require("lazy").setup({
+	{ import = "sauce.plugins" },
+	{ import = "sauce.plugins.lsp" },
+	{ import = "sauce.plugins.colorscheme" },
+	{ import = "sauce.plugins.mcp" },
+}, {
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		-- notify = false,
+	},
+})
