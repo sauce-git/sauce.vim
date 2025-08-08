9674bed0800275431c0f87ca544ecd87fbe6c1cd
sauce <dadad1004@naver.com>
2025-08-08 18:38:47 +0900

    chore: update rename keymap

--- lua/plugins/inc-rename.lua
+++ lua/plugins/inc-rename.lua
@@ -3,7 +3,14 @@ return {
     "smjonas/inc-rename.nvim",
     cmd = "IncRename",
     keys = {
-      { "<leader>rn", ":IncRename ", desc = "Rename" },
+      {
+        "<leader>rn",
+        function()
+          return ":IncRename " .. vim.fn.expand("<cWORD>")
+        end,
+        expr = true,
+        desc = "Rename under cursor",
+      },
     },
     opts = {
       -- the name of the command
