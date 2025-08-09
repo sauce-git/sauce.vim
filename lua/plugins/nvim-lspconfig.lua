010a0ce70ba8517a3d406a99f97149ef0194c38b
sauce <dadad1004@naver.com>
2025-08-09 11:19:16 +0900

    chore: update keymap

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -4,6 +4,7 @@ return {
     opts = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = { "<leader>rs", "<cmd>LspRestart<cr>", desc = "Restart LSP" }
+      keys[#keys + 1] = { "<C-k>", false, mode = { "i" }, desc = "Signature Help" }
     end,
   },
 }
