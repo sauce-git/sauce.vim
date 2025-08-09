cd16258d0b2efb3e0ab9d1aaf334a62938be1445
sauce <dadad1004@naver.com>
2025-08-09 12:01:31 +0900

    chore: update rename plugin

--- lua/plugins/inc-rename.lua
+++ lua/plugins/inc-rename.lua
@@ -2,16 +2,6 @@ return {
   {
     "smjonas/inc-rename.nvim",
     cmd = "IncRename",
-    keys = {
-      {
-        "<leader>rn",
-        function()
-          return ":IncRename " .. vim.fn.expand("<cWORD>")
-        end,
-        expr = true,
-        desc = "Rename under cursor",
-      },
-    },
     opts = {
       -- the name of the command
       cmd_name = "IncRename",
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,6 +5,16 @@ return {
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = { "<leader>rs", "<cmd>LspRestart<cr>", desc = "Restart LSP" }
       keys[#keys + 1] = { "<C-k>", false, mode = { "i" }, desc = "Signature Help" }
+      keys[#keys + 1] = {
+        "<leader>rn",
+        function()
+          local inc_rename = require("inc_rename")
+          return ":" .. inc_rename.config.cmd_name .. " " .. vim.fn.expand("<cword>")
+        end,
+        expr = true,
+        desc = "Rename (inc-rename.nvim)",
+        has = "rename",
+      }
     end,
   },
 }
