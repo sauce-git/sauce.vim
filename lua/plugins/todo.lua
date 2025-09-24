187d4a9620b7bf1cea4306b7ea468228b4fc8f5d
sauce-git <dadad02100@gmail.com>
2025-09-25 01:51:39 +0900

    refactor: seperate todo from trouble

---
+++ lua/plugins/todo.lua
@@ -0,0 +1,23 @@
+vim.pack.add({
+  {
+    src = "https://github.com/folke/trouble.nvim.git",
+    name = "trouble.nvim",
+  },
+  {
+    src = "https://github.com/folke/todo-comments.nvim.git",
+    name = "todo-comments.nvim",
+  },
+})
+
+vim.defer_fn(function()
+  local todo_comments_ok, todo_comments = pcall(require, "todo-comments")
+  if not todo_comments_ok then
+    vim.notify("todo-comments.nvim not found", vim.log.levels.ERROR)
+    return
+  end
+  todo_comments.setup({})
+
+  vim.keymap.set("n", "<leader>xt", "<cmd>TodoTrouble<cr>", { desc = "Todo (Telescope)" })
+
+  print("todo-comments.nvim loaded")
+end, 50)
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,16 +1,11 @@
 vim.pack.add({
   {
-    src = "https://github.com/folke/todo-comments.nvim.git",
-    name = "todo-comments.nvim",
-  },
-  {
     src = "https://github.com/folke/trouble.nvim.git",
     name = "trouble.nvim",
   },
 })
 
 vim.defer_fn(function()
-  -- Trouble.nvim
   local trouble_ok, trouble = pcall(require, "trouble")
   if not trouble_ok then
     vim.notify("trouble.nvim not found", vim.log.levels.ERROR)
@@ -36,16 +31,4 @@ vim.defer_fn(function()
   vim.keymap.set("n", "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", { desc = "Quickfix List (Trouble)" })
 
   print("trouble.nvim loaded")
-
-  -- todo-comments.nvim
-  local todo_comments_ok, todo_comments = pcall(require, "todo-comments")
-  if not todo_comments_ok then
-    vim.notify("todo-comments.nvim not found", vim.log.levels.ERROR)
-    return
-  end
-  todo_comments.setup({})
-
-  vim.keymap.set("n", "<leader>xt", "<cmd>TodoTrouble<cr>", { desc = "Todo (Telescope)" })
-
-  print("todo-comments.nvim loaded")
 end, 50)
