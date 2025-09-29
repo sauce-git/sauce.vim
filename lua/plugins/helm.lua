10b5091c0b152e70d9f744fe0769e73bae080b8e
sauce-git <dadad02100@gmail.com>
2025-09-29 14:06:53 +0900

    chore: style update

--- lua/plugins/helm.lua
+++ lua/plugins/helm.lua
@@ -8,19 +8,18 @@ vim.pack.add({
 -- Load vim-helm after treesitter to prevent conflicts
 vim.defer_fn(function()
   -- Set up Helm file detection and settings
-  vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-    pattern = {"*.yaml", "*.yml"},
+  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { "*.yaml", "*.yml" },
     callback = function()
       local path = vim.fn.expand("%:p")
       local filename = vim.fn.expand("%:t")
 
       -- Check if it's a Helm file by path or content
       if string.match(path, "templates/") or
-         string.match(path, "charts/") or
-         string.match(path, "helm") or
-         string.match(filename, "values") or
-         string.match(filename, "Chart") then
-
+          string.match(path, "charts/") or
+          string.match(path, "helm") or
+          string.match(filename, "values") or
+          string.match(filename, "Chart") then
         vim.opt_local.backup = false
         vim.opt_local.writebackup = false
         vim.opt_local.swapfile = false
@@ -39,3 +38,4 @@ vim.defer_fn(function()
     end,
   })
 end, 100) -- Load with delay after treesitter
+
