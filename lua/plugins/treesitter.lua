79b7a0a42590738c6a24a4d257aeeb70cd1c5893
sauce <sauce@sauce.local>
2026-01-12 22:57:05 +0900

    refactor: update treesitter plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,41 +9,41 @@ vim.defer_fn(function()
   local ok, treesitter = pcall(require, "nvim-treesitter")
   if not ok then
     vim.notify("nvim-treesitter not found", vim.log.levels.ERROR)
+    return
   end
 
-  treesitter.setup({
-    ensure_installed = {
-      "c",
-      "cpp",
-      "go",
-      "lua",
-      "python",
-      "rust",
-      "typescript",
-      "javascript",
-      "html",
-      "css",
-      "json",
-      "yaml",
-      "toml",
-      "markdown",
-      "bash",
-    },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = {
-      enable = true,
-    },
-    autotag = {
-      enable = true,
-    },
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    },
+  -- Install parsers
+  local languages = {
+    "c",
+    "cpp",
+    "go",
+    "lua",
+    "python",
+    "rust",
+    "typescript",
+    "javascript",
+    "html",
+    "css",
+    "json",
+    "yaml",
+    "toml",
+    "markdown",
+    "bash",
+  }
+
+  treesitter.install(languages)
+
+  -- Enable highlighting for all filetypes
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function()
+      local buf = vim.api.nvim_get_current_buf()
+      if not vim.b[buf].treesitter_enabled then
+        pcall(vim.treesitter.start)
+        vim.b[buf].treesitter_enabled = true
+      end
+    end,
   })
+
   vim.notify("nvim-treesitter loaded", vim.log.levels.INFO)
 end, 0)
