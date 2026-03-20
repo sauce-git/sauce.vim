37db3f5ae4ed51de4fdffbc321184ac866c1c57b
sauce-git <sauce@mandacode.com>
2026-03-20 21:34:18 +0900

    refactor: move to opencode

---
+++ lua/plugins/opencode.lua
@@ -0,0 +1,23 @@
+vim.pack.add({
+  {
+    src = "https://github.com/nickjvandyke/opencode.nvim.git",
+    name = "opencode.nvim",
+  },
+})
+
+vim.defer_fn(function()
+  vim.g.opencode_opts = {}
+
+  vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end,
+    { desc = "Ask opencode" })
+  vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end, { desc = "Execute opencode action" })
+  vim.keymap.set({ "n", "t" }, "<leader>aa", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
+  vim.keymap.set({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end,
+    { desc = "Add range to opencode", expr = true })
+  vim.keymap.set("n", "goo", function() return require("opencode").operator("@this ") .. "_" end,
+    { desc = "Add line to opencode", expr = true })
+  vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,
+    { desc = "Scroll opencode up" })
+  vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end,
+    { desc = "Scroll opencode down" })
+end, 50)
